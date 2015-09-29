ba2e6df07929fb75f12cef25eb5652ed2cda850d
anaGodoy <ana.godoy@ccc.ufcg.edu.br>
2015-09-29 13:45:21 -0400

    template of the Matrix Class

--- lab03/src/lab03/Matrix.java
+++ lab03/src/lab03/Matrix.java
@@ -1,5 +1,42 @@
 package lab03;
 
+import java.io.File;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class Matrix {
+private File file; 
+private ArrayList<Array> result;
+	public Matrix(File file ) {
+		this.file = file;
+		result= new ArrayList<Array>();
+		
+	}
+	public void buildMatriX(){
+		
+	}
+	@Override
+	public String toString() {
+		
+		return "Matrix [result=" + result + "]";
+	}
+	public File getFile() {
+		return file;
+	}
+	public void setFile(File file) {
+		this.file = file;
+	}
+	
+	public void multiply(){
+		
+	}
+	
+	
+	public static void main(String[] args) {
+		
+	}
+	
+	
+	
 
 }
